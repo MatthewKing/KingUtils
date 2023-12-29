964ae98d736cd9fc6e26878844862c82da6eeb02
Matthew King <mking@mking.net>
2023-12-29 12:29:17 +0800

    Add Guid ToBase32String extension method.

---
+++ src/KingUtils/GuidExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace KingUtils;
+
+/// <summary>
+/// Extension methods for GUIDs.
+/// </summary>
+public static class GuidExtensions
+{
+    /// <summary>
+    /// Returns a base-32 string representation of the GUID.
+    /// Uses Crockford base-32 by default.
+    /// </summary>
+    /// <param name="guid">The GUID to format.</param>
+    /// <returns>A base-32 string representation of the GUID.</returns>
+    public static string ToBase32String(this Guid guid)
+    {
+        return ToBase32String(guid, Base32.Crockford);
+    }
+
+    /// <summary>
+    /// Returns a base-32 string representation of the GUID.
+    /// </summary>
+    /// <param name="guid">The GUID to format.</param>
+    /// <param name="base32Encoder">The base-32 encoder to use.</param>
+    /// <returns>A base-32 string representation of the GUID.</returns>
+    public static string ToBase32String(this Guid guid, IBase32 base32Encoder)
+    {
+#if NET7_0_OR_GREATER
+        Span<byte> bytes = stackalloc byte[16];
+        guid.TryWriteBytes(bytes);
+        return base32Encoder.Encode(bytes);
+#else
+        return base32Encoder.Encode(guid.ToByteArray());
+#endif
+    }
+}
+
